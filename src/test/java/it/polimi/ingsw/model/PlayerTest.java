bf47d86b314ae8b67f4687ea6d10bc5821b1771b
Vito96 <svait96@hotmail.com>
2020-03-29 14:49:00 +0200

    Implemented PlayerTest and WorkerTest

--- src/test/java/it/polimi/ingsw/model/PlayerTest.java
+++ src/test/java/it/polimi/ingsw/model/PlayerTest.java
@@ -1,4 +1,41 @@
 package it.polimi.ingsw.model;
 
+import it.polimi.ingsw.commons.Colors;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class PlayerTest {
+    private Player player;
+    private String nameOfThePlayer = "NAME";
+    private Date birthdayOfThePlayer = new Date();
+
+    @Before
+    public void setUp() {
+        this.player = new Player(nameOfThePlayer, birthdayOfThePlayer);
+    }
+
+    @Test
+    public void getName() {
+        Assert.assertEquals(player.getName(), nameOfThePlayer);
+    }
+
+    @Test
+    public void getBirthday() {
+        Assert.assertEquals(player.getBirthday(), birthdayOfThePlayer);
+    }
+
+    @Test
+    public void checkCreationAndGettingOfTheWorkers() {
+        List<Worker> localWorkers = new ArrayList();
+        localWorkers.add(player.addWorker(Colors.BLUE));
+        localWorkers.add(player.addWorker(Colors.BLUE));
+        List<Worker> listOfWorkersOfPlayer = player.getWorkers();
+        Assert.assertTrue(listOfWorkersOfPlayer.containsAll(localWorkers) && localWorkers.size() == listOfWorkersOfPlayer.size());
+    }
 }
--- src/test/java/it/polimi/ingsw/model/WorkerTest.java
+++ src/test/java/it/polimi/ingsw/model/WorkerTest.java
@@ -1,4 +1,29 @@
 package it.polimi.ingsw.model;
 
+import it.polimi.ingsw.commons.Colors;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class WorkerTest {
+    private Worker worker;
+    private Colors colorBlue = Colors.BLUE;
+
+    @Before
+    public void setUp() {
+        worker = new Worker(colorBlue);
+    }
+
+    @Test
+    public void getColor() {
+        Assert.assertSame(worker.getColor(), colorBlue);
+    }
+
+    @Test
+    public void setAndGetSameCurrentCell() {
+        Cell currentCell = new Cell(1, 1);
+        worker.setCurrentCell(currentCell);
+        Assert.assertEquals(worker.getCurrentCell(), currentCell);
+    }
 }
