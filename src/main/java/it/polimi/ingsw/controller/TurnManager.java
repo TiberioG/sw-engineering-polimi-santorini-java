d0dd687e9f8879da2621b718a59df5433a91418c
Vito96 <svait96@hotmail.com>
2020-04-05 01:10:11 +0200

    Use reflection in turnManager to set strategy class from strategySettings object

--- src/main/java/it/polimi/ingsw/controller/TurnManager.java
+++ src/main/java/it/polimi/ingsw/controller/TurnManager.java
@@ -1,40 +1,58 @@
 package it.polimi.ingsw.controller;
 
+import it.polimi.ingsw.controller.strategies.strategyBuild.StrategyBuild;
+import it.polimi.ingsw.controller.strategies.strategyMove.StrategyMove;
+import it.polimi.ingsw.controller.strategies.strategyWin.StrategyWin;
 import it.polimi.ingsw.model.*;
+
+import java.lang.reflect.Constructor;
 
 public class TurnManager {
     private Match match;
     private Player currentPlayer;
+    private CardManager cardManager;
     //private HashMap<Worker, Cell> initialCellMap = new HashMap<>();
     //private Virtual View
 
     //various strategy
+    private StrategyMove strategyMove;
+    private StrategyWin strategyWin;
+    private StrategyBuild strategyBuild;
 
-
+    private Object createStrategyWithReflection(String className, Class[] types, Object[] parameters) throws Exception {
+            //Class.forName("it.polimi.ingsw.controller.strategies.strategyMove." + card.getStrategySettings().getStrategyMove());
+            Class clazz = Class.forName(className);
+            Constructor constructor =  clazz.getConstructor(types);
+            return constructor.newInstance(parameters);
+    }
 
     private void buildStrategies() {
         //builder/factory
-        //String id = this.currentPlayer.getCurrentCard();
-        //StrategyJsonMapper strategyJsonMapper = CardManager.getCardInfo(id)
+        int cardId = this.currentPlayer.getCurrentCard();
+        Card card = cardManager.getCardById(cardId);
 
         //use reflection
-        //Class<AvailableCell> class = Class.forName("it.polimi.ingsw.controller.strategies" + strategyJsonMapper.strategyRetrieveCellForMove);
-        //Constructor<?> constructor = clazz.getConstructor(String.class, Integer.class);
-        //Object instance = constructor.newInstance("stringparam", 42);
-
-        //make this for all strategies
+        try {
+            //make this for all strategies
+            strategyMove = (StrategyMove) createStrategyWithReflection("it.polimi.ingsw.controller.strategies.strategyMove." + card.getStrategySettings().getStrategyMove(), new Class[]{Match.class}, new Object[]{match});
+            strategyWin = (StrategyWin) createStrategyWithReflection("it.polimi.ingsw.controller.strategies.strategyWin." + card.getStrategySettings().getStrategyWin(), new Class[]{Match.class}, new Object[]{match});
+            //strategyBuild = (StrategyBuild) createStrategyWithReflection("it.polimi.ingsw.controller.strategies.strategyBuild." + card.getStrategySettings().getStrategyBuild(), new Class[]{Match.class}, new Object[]{match});
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     public TurnManager(Match match) {
         this.match = match;
         this.currentPlayer = this.match.getCurrentPlayer();
+        this.cardManager = CardManager.initCardManager();
         buildStrategies();
         TurnProperties.resetAllParameter();
     }
 
     public void beginTurn() {
         currentPlayer.getWorkers().forEach(worker -> {
-            TurnProperties.getInitialPositionMap().put(worker, worker.getCurrentCell());
+            TurnProperties.getInitialPositionMap().put(worker, this.match.getLocation().getLocation(worker));
         });
     };
 }
