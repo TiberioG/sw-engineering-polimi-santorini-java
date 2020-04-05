a59e4f554f7733f6f73a20ea3d577f2109ea869d
Vito96 <svait96@hotmail.com>
2020-04-05 18:07:12 +0200

    Create JavaDoc and clean code of JsonAdapter, CardManager, StrategySetting and TurnManager classes

--- src/main/java/it/polimi/ingsw/commons/JsonAdapter.java
+++ src/main/java/it/polimi/ingsw/commons/JsonAdapter.java
@@ -8,15 +8,31 @@ import com.google.gson.stream.JsonReader;
 
 import java.io.InputStreamReader;
 
+/**
+ * This is an adapter class for {@link JsonReader} class and {@link Gson} class
+ * @author Vito96
+ */
 public class JsonAdapter {
+    /* Attributes */
     private JsonReader jsonReader;
     private JsonObject mainJsonObject;
     private JsonArray mainJsonArray;
 
+    /* Constructor(s) */
+
+    /**
+     * Constructor: init jsonReader class
+     * @param pathOfFile the relative path of the json file
+     */
     public JsonAdapter(String pathOfFile) {
         jsonReader = new JsonReader(new InputStreamReader(getClass().getResourceAsStream(pathOfFile)));
     }
 
+    /**
+     * Constructor: init jsonReader class and init the first structure of the json file
+     * @param pathOfFile the relative path of the json file
+     * @param typeOfFirstNode the type of the first structure of the json file
+     */
     public JsonAdapter(String pathOfFile, String typeOfFirstNode) {
         this(pathOfFile);
         if (typeOfFirstNode.equals("array")) {
@@ -26,14 +42,31 @@ public class JsonAdapter {
         }
     }
 
+    /* Methods */
+
+    /**
+     * This method return the mainJsonObject
+     * @return the mainJsonObject of json file
+     */
     public JsonObject getMainJsonObject() {
         return mainJsonObject;
     }
 
+    /**
+     * This method return the mainJsonArray
+     * @return the mainJsonArray of json file
+     */
     public JsonArray getMainJsonArray() {
         return mainJsonArray;
     }
 
+    /**
+     * return from the {@link JsonObject} the string value of a specified key or return a defaultValue if the key is not present
+     * @param jsonObject
+     * @param keyValue the key of the desired value
+     * @param defaultValue the return value if the key is not present
+     * @return the value of the key
+     */
     public static String getStringFromJsonObject(JsonObject jsonObject, String keyValue, String defaultValue) {
         JsonElement jsonElement = jsonObject.get(keyValue);
         if (jsonElement != null) {
@@ -41,6 +74,12 @@ public class JsonAdapter {
         } else return defaultValue;
     };
 
+    /**
+     * return from the {@link JsonObject} the string value of a specified key
+     * @param jsonObject
+     * @param keyValue the key of the desired value
+     * @return the value of the key
+     */
     public static String getStringFromJsonObject(JsonObject jsonObject, String keyValue) {
         JsonElement jsonElement = jsonObject.get(keyValue);
         if (jsonElement != null) {
@@ -48,6 +87,13 @@ public class JsonAdapter {
         } else return null;
     };
 
+    /**
+     * return from the {@link JsonObject} the int value of a specified key or return a defaultValue if the key is not present
+     * @param jsonObject
+     * @param keyValue the key of the desired value
+     * @param defaultValue the return value if the key is not present
+     * @return the value of the key
+     */
     public static int getIntFromJsonObject(JsonObject jsonObject, String keyValue, int defaultValue) {
         JsonElement jsonElement = jsonObject.get(keyValue);
         if (jsonElement != null) {
@@ -55,6 +101,12 @@ public class JsonAdapter {
         } else return defaultValue;
     };
 
+    /**
+     * return from the {@link JsonObject} the int value of a specified key
+     * @param jsonObject
+     * @param keyValue the key of the desired value
+     * @return the value of the key
+     */
     public static int getIntFromJsonObject(JsonObject jsonObject, String keyValue) {
         JsonElement jsonElement = jsonObject.get(keyValue);
         if (jsonElement != null) {
