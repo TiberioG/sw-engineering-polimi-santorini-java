746c1471fe81463b69f31702a84adc18c1fae15b
TiberioG <tiberio.galbiati@gmail.com>
2020-04-05 18:21:10 +0200

    New buildStrategies, modular version

--- src/main/java/it/polimi/ingsw/controller/strategies/strategyBuild/DefaultBuild.java
+++ src/main/java/it/polimi/ingsw/controller/strategies/strategyBuild/DefaultBuild.java
@@ -1,6 +1,8 @@
 package it.polimi.ingsw.controller.strategies.strategyBuild;
 
 import it.polimi.ingsw.commons.Component;
+import it.polimi.ingsw.controller.TurnManager;
+import it.polimi.ingsw.controller.TurnProperties;
 import it.polimi.ingsw.exceptions.*;
 import it.polimi.ingsw.model.*;
 
@@ -9,53 +11,64 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class DefaultBuild implements StrategyBuild{
+public class DefaultBuild implements StrategyBuild {
+    /* Attributes */
+    private Match match;
 
-    /*
-    public void buildComponent(Cell cell) {
-        Tower currentTower = cell.getTower();
-        try {
-            currentTower.addComponent(component);
-        } catch (BuildLowerComponentException e) {
-            e.printStackTrace();
-        }
+
+    /* Constructor(s) */
+    public DefaultBuild(Match match) {
+        this.match = match;
     }
-    */
 
+    /* Methods */
     @Override
-    public void build(ArrayList<Component> listCompToBuild, ArrayList<Cell> listWhereToBuild, Match match, Worker worker) {
-        Cell whereIam = match.getLocation().getLocation(worker);
-        ArrayList nearby = match.getIsland().getAdjCells(whereIam);
+    public void build(Component compToBuild, Cell whereToBuild, Worker worker) throws ZeroCellsAvailableBuildException, WrongCellSelectedBuildException, ComponentNotAllowed, BuildLowerComponentException {
+        List<Cell> buildableCells = getBuildableCells(worker);
 
-
-        //0check, since is default strategy i con only build once
-        boolean cond0 = ( listCompToBuild.size() == 1 && listWhereToBuild.size() == 1 );
-
-        //1st check, I should only build close to me
-        boolean cond1 = nearby.contains(listWhereToBuild.get(0));
-
-        //2nd check, where I wanna build there's not another worker
-        boolean cond2 = (match.getLocation().getOccupant(listWhereToBuild.get(0)) == null);
-
-        //3rd check, what I wanna build is compatible with the level already present
-        boolean cond3 = listWhereToBuild.get(0).getTower().nextBuildable() == listCompToBuild.get(0).getComponentCode();
-
-        //TODO gestire le eccezioni per ogni condizione
-
-        if (cond0 && cond1 && cond2 && cond3){
-            try {
-                listWhereToBuild.get(0).getTower().addComponent(listCompToBuild.get(0));
-            } catch (BuildLowerComponentException e) {
-                e.printStackTrace();
+        if(buildableCells.size() == 0) //1 check: threre are free cells close to the worker where is possible to build
+        {
+            throw new ZeroCellsAvailableBuildException();
+        }
+        else {
+            if(!buildableCells.contains(whereToBuild) ) //2 check: where I wanna build is in the list of buildable cells
+            {
+                throw new WrongCellSelectedBuildException();
             }
-
+            else {
+                if(!getComponentsBuildable(whereToBuild).contains(compToBuild.getComponentCode()) ) //3 check: what I wanna bubild is allowed there
+                {
+                    throw new ComponentNotAllowed();
+                }
+                whereToBuild.getTower().addComponent(compToBuild);
+                TurnProperties.builtNow(worker, whereToBuild);
+            }
         }
 
-
-
-
-
-
-
     }
+
+    private List<Cell> getBuildableCells(Worker worker) {
+        Cell whereIam = match.getLocation().getLocation(worker);
+        List<Cell> adjCells = match.getIsland().getAdjCells(whereIam);
+        return adjCells.stream()
+                .filter(cell -> cell.getTower().getTopComponent() != Component.DOME) // remove cells where the tower is complete
+                .filter(cell -> match.getLocation().getOccupant(cell) == null) // removes cells where there is a worker
+                .collect(Collectors.toList());
+    }
+
+    private List<Integer>getComponentsBuildable(Cell cell){
+        List<Integer> comps = new ArrayList<>();
+        Component current = cell.getTower().getTopComponent();
+        if (current == Component.DOME){ // if tower is already complete
+            return comps; // the list of buildable components must be empty
+        }
+        else {
+            comps.add(current.getComponentCode() + 1);
+            return comps;
+        }
+    }
+
+
 }
+
+
--- src/main/java/it/polimi/ingsw/controller/strategies/strategyBuild/DomeAnywhere.java
+++ src/main/java/it/polimi/ingsw/controller/strategies/strategyBuild/DomeAnywhere.java
@@ -1,40 +1,68 @@
 package it.polimi.ingsw.controller.strategies.strategyBuild;
 
 import it.polimi.ingsw.commons.Component;
+import it.polimi.ingsw.controller.TurnProperties;
 import it.polimi.ingsw.exceptions.BuildLowerComponentException;
+import it.polimi.ingsw.exceptions.ComponentNotAllowed;
+import it.polimi.ingsw.exceptions.WrongCellSelectedBuildException;
+import it.polimi.ingsw.exceptions.ZeroCellsAvailableBuildException;
 import it.polimi.ingsw.model.Cell;
 import it.polimi.ingsw.model.Match;
 import it.polimi.ingsw.model.Worker;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 // ATLANTE
 
 public class DomeAnywhere implements StrategyBuild {
+    /* Attributes */
+    private Match match;
+
+
+    /* Methods */
     @Override
-    public void build(ArrayList<Component> listCompToBuild, ArrayList<Cell> listWhereToBuild, Match match, Worker worker) {
-        Cell whereIam = match.getLocation().getLocation(worker);
-        ArrayList nearby = match.getIsland().getAdjCells(whereIam);
+    public void build(Component compToBuild, Cell whereToBuild, Worker worker) throws ZeroCellsAvailableBuildException, WrongCellSelectedBuildException, ComponentNotAllowed, BuildLowerComponentException {
+        List<Cell> buildableCells = getBuildableCells(worker);
 
-        //0check, atlante can only build once
-        boolean cond0 = (listCompToBuild.size() == 1 && listWhereToBuild.size() == 1);
-
-        //1st check, I should only build close to me
-        boolean cond1 = nearby.contains(listWhereToBuild.get(0));
-
-        //2nd check, where I wanna build there's not another worker
-        boolean cond2 = (match.getLocation().getOccupant(listWhereToBuild.get(0)) == null);
-
-        //no more check next thing buildable in list
-
-        //TODO gestire le eccezioni per ogni condizione
-
-        if (cond0 && cond1 && cond2) {
-            try {
-                listWhereToBuild.get(0).getTower().addComponent(listCompToBuild.get(0));
-            } catch (BuildLowerComponentException e) {
-                e.printStackTrace();
+        if (buildableCells.size() == 0) //1 check: threre are free cells close to the worker where is possible to build
+        {
+            throw new ZeroCellsAvailableBuildException();
+        } else {
+            if (!buildableCells.contains(whereToBuild)) //2 check: where I wanna build is in the list of buildable cells
+            {
+                throw new WrongCellSelectedBuildException();
+            } else {
+                if (!getComponentsBuildable(whereToBuild).contains(compToBuild.getComponentCode())) //3 check: what I wanna bubild is allowed there
+                {
+                    throw new ComponentNotAllowed();
+                }
+                whereToBuild.getTower().addComponent(compToBuild);
+                TurnProperties.builtNow(worker, whereToBuild);
             }
+        }
+
+    }
+
+    private List<Cell> getBuildableCells(Worker worker) {
+        Cell whereIam = match.getLocation().getLocation(worker);
+        List<Cell> adjCells = match.getIsland().getAdjCells(whereIam);
+        return adjCells.stream()
+                .filter(cell -> cell.getTower().getTopComponent() != Component.DOME) // remove cells where the tower is complete
+                .filter(cell -> match.getLocation().getOccupant(cell) == null) // removes cells where there is a worker
+                .collect(Collectors.toList());
+    }
+
+    private List<Integer> getComponentsBuildable(Cell cell) {
+        List<Integer> comps = new ArrayList<>();
+        Component current = cell.getTower().getTopComponent();
+        if (current == Component.DOME) { // if tower is already complete
+            return comps; // the list of buildable components must be empty
+        } else {
+            comps.add(current.getComponentCode() + 1);
+            comps.add(Component.DOME.getComponentCode()); // a dome is allowed everywhere
+            return comps;
         }
     }
 }
