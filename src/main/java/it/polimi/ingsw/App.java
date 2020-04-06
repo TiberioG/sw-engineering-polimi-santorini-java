413f97dffe87fb1a7e9a2f98cb762c44664bca85
TiberioG <tiberio.galbiati@gmail.com>
2020-04-06 22:48:38 +0200

    generated coverage, fix date and test App add

--- src/main/java/it/polimi/ingsw/App.java
+++ src/main/java/it/polimi/ingsw/App.java
@@ -4,10 +4,15 @@ package it.polimi.ingsw;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
+
         System.out.println( "Hello World!" );
+    }
+
+    public static boolean foo (){
+        return true;
     }
 }
