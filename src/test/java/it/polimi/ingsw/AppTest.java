dcb16b6ce78c667301f8272298f5c49170208682
TiberioG <tiberio.galbiati@gmail.com>
2020-04-06 22:48:38 +0200

    generated coverage
    generated coverage, fix date and test App add

--- src/test/java/it/polimi/ingsw/AppTest.java
+++ src/test/java/it/polimi/ingsw/AppTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
+public class AppTest
 {
     /**
      * Rigorous Test :-)
@@ -15,6 +15,7 @@ public class AppTest
     @Test
     public void shouldAnswerWithTrue()
     {
-        assertTrue( true );
+        boolean boo = App.foo();
+        assertTrue(boo);
     }
 }
